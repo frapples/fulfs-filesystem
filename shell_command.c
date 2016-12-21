8381112442c165b29ef9bcb5f88c3d0c761e2373
Frapples <isfrapples@outlook.com>
2016-12-21 23:12:34 +0800

    为了方便测试，实现几个小命令

--- shell_command.c
+++ shell_command.c
@@ -10,22 +10,61 @@ int cmd_pwd(int argc, char* argv[])
 
 int cmd_cd(int argc, char* argv[])
 {
-    printf("%d\n", argc);
-
-    for (int i = 0; i < argc; i++) {
-        printf("[%s]\n", argv[i]);
+    if (argc <= 0) {
+        printf("命令有误\n");
+        return -1;
     }
-    return 0;
+
+    if (fs_chdir(argv[0]) == FS_SUCCESS) {
+        return 0;
+    } else {
+        printf("改变失败，可能目录不存在\n");
+        return -1;
+    }
+
 }
 
 int cmd_ls(int argc, char* argv[])
 {
+    const char* path;
+    char wd[FS_MAX_FILE_PATH];
+    if (argc <= 0) {
+        fs_getcwd(wd, FS_MAX_FILE_PATH);
+        path = wd;
+    } else {
+        path = argv[0];
+    }
+
+    FS_DIR* dir = fs_opendir(path);
+    if (dir == NULL) {
+        printf("列出目录信息失败\n");
+    }
+
+    char name[32];
+    do {
+        fs_readdir(dir, name);
+        printf("%s ", name);
+    } while(name[0] != '\0');
+    printf("\n");
+
+    fs_closedir(dir);
+
     return 0;
 }
 
 int cmd_mkdir(int argc, char* argv[])
 {
-    return 0;
+    if (argc <= 0) {
+        printf("命令有误\n");
+        return -1;
+    }
+
+    if (fs_mkdir(argv[0]) == FS_SUCCESS) {
+        return 0;
+    } else {
+        printf("建立失败\n");
+        return -1;
+    }
 }
 
 int cmd_rmdir(int argc, char* argv[])
